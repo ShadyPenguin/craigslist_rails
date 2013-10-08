380312a15c5ebb5f650dc14964d290f58d84f376
Jake Sikora <jakesikora.01@gmail.com>
2013-10-07 23:05:25 -0500

    User creation view working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,26 @@ class UsersController < ApplicationController
   def index
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to user_path(user.id)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
